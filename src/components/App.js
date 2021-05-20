bc12518eae65e8a01544690e9d2e0a4958c20d2b
Matthew <elijah2895@hotmail.com>
2021-05-20 16:57:09 -0400

    initial setup of most of the components needed

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Home from "./Home";
+import PostContainer from "./PostContainer";
+import Profile from "./Profile";
+
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;

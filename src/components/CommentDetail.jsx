2bfc24097657c3b72ad26f88ef5553a6fb7bbbc8
Matthew E Newton <Elijah2895@hotmail.com>
2022-02-20 21:26:39 -0500

    Update CommentDetail.jsx

--- src/components/CommentDetail.jsx
+++ src/components/CommentDetail.jsx
@@ -42,7 +42,7 @@ function CommentDetail ({comment, user, commentUserId, commentUser, commentRemov
     }
 
     function handleDelete() {
-        fetch(`https://nameless-tor-13132.herokuapp.com/comments/${id}`, {
+        fetch(`https://ink-gaming.herokuapp.com/comments/${id}`, {
             method: 'DELETE'
         })
         setEditComment(false)
@@ -51,7 +51,7 @@ function CommentDetail ({comment, user, commentUserId, commentUser, commentRemov
 
     function handleSubmit(e) {
         e.preventDefault()
-        fetch(`https://nameless-tor-13132.herokuapp.com/comments/${id}`, {
+        fetch(`https://ink-gaming.herokuapp.com/comments/${id}`, {
             method: 'PATCH',
             headers: {
                 "Content-Type": 'application/json',
